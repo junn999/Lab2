3542da6d190a4e35c33d4f4a9a055a6f171fa9e7
junn999 <junioralberto1421@gmail.com>
2024-04-07 22:37:16 -0600

    Commit inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home_view.css">
+    <title>Home</title>
+</head>
+<body>
+    <div>
+        <h1>Bienvenido!!!</h1>
+
+        <p>Nuestra página aún está en construcción</p>
+        <div align='center'>
+            <img alighn src="https://i.pinimg.com/originals/ce/d2/d0/ced2d0cc1832708a6a1ee95df0e285a1.gif" width="300" height="200">
+            <img src="https://th.bing.com/th/id/R.265dc176e68bc6527e8a9528958872b4?rik=6u1JMVZe5xed3w&riu=http%3a%2f%2fi.huffpost.com%2fgadgets%2fslideshows%2f337638%2fslide_337638_3430097_free.gif&ehk=2RV4ZowjRBQuWL5%2fVuI0ZcIy4r3aWXtmfJh567%2bfIuM%3d&risl=&pid=ImgRaw&r=0" width="300" height="200">  
+            
+        </div>
+       <h3>
+            ¿Qué es Lorem Ipsum?
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
+            Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, 
+            cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una 
+            galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. 
+            No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos 
+            electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la 
+            creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más 
+            recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye 
+            versiones de Lorem Ipsum.
+        </h3>
+        <a href="{{ url('/about') }}">Ir a About</a> 
+    </div>   
+
+
+</body>
+</html>
